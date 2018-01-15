51fe55538df65bb7d7e4331ba1d7940bfcc7bb17
Alexander Kryuk <kryukalexander@gmail.com>
2018-01-15 15:50:10 +0600

    WIP base connect

---
+++ src/components/todo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>{{test}}</h1>
+        <ul>
+            <li v-for="todo in todos" >{{ todo.text }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Firebase from 'firebase';
+
+    let config = {
+        apiKey: "AIzaSyC4hmBR-OcnaGtkLe8O72uZfLpf1VeSuMg",
+        authDomain: "test-todo-app-c7154.firebaseapp.com",
+        databaseURL: "https://test-todo-app-c7154.firebaseio.com",
+        projectId: "test-todo-app-c7154",
+        storageBucket: "test-todo-app-c7154.appspot.com",
+        messagingSenderId: "563346292110"
+    };
+
+    let app = Firebase.initializeApp(config);
+    let db = app.database();
+    let todos = db.ref('todo-list');
+
+    export default {
+        name: 'todo',
+        data () {
+            return {
+                todos: todos,
+                test: 'It works'
+            }
+        },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+import Router from 'vue-router'
+import todo from '@/components/todo'
+
+Vue.use(Router);
 
 export default new Router({
   routes: [
+
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+        path: '/',
+        name: 'todo',
+        component: todo
     }
   ]
 })
